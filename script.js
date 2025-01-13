8e306d82c0e1a928fc8110e33893c80367363113
royalcala05 <roycs21100@gmail.com>
2025-01-13 13:55:51 -0500

    update screens and create js file

---
+++ script.js
@@ -0,0 +1,18 @@
+
+// Check if the "Start Game" button exists on the current page
+const startGameButton = document.getElementById('guestPlay-button');
+if (startGameButton) {
+    // Add a click event to navigate to playScreen.html
+    startGameButton.addEventListener('click', () => {
+        window.location.href = 'playScreen.html'; // Navigate to the play screen
+    });
+}
+
+// Check if the "Go Back" button exists on the current page
+// const goBackButton = document.getElementById('goBackButton');
+// if (goBackButton) {
+//     // Add a click event to navigate back to index.html
+//     goBackButton.addEventListener('click', () => {
+//         window.location.href = 'loadScreen.html'; // Navigate back to the load screen
+//     });
+// }
